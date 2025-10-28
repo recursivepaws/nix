3dbf392da36753d5b0358412fe20b96a081e3dfd
Vera Gonzalez <me@vera.lgbt>
2025-10-28 13:51:04 -0400

    gtk customization

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -2,21 +2,31 @@
   gtk = {
     enable = true;
     theme = {
-      name = "Gruvbox-Material-Dark";
-      package =
-        inputs.nixpkgs.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.pkgs.gruvbox-material-gtk-theme;
+      name = "Dracula";
+      package = pkgs.dracula-theme;
     };
     cursorTheme = {
-      name = "Capitaine Cursors (Gruvbox)";
-      package =
-        inputs.nixpkgs.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.capitaine-cursors-themed;
+      name = "Dracula-cursors";
+      package = pkgs.dracula-theme;
+      size = 16;
     };
+    # iconTheme = {
+    #   name = "Dracula-icons";
+    #   package = pkgs.dracula-theme;
+    # };
     iconTheme = {
-      name = "Gruvbox-Plus-Dark";
-      package =
-        inputs.nixpkgs.outputs.legacyPackages.${pkgs.stdenv.hostPlatform.system}.gruvbox-plus-icons;
+      name = "Papirus-Dark";
+      package = pkgs.papirus-icon-theme;
     };
+    gtk3.extraConfig = { gtk-application-prefer-dark-theme = 1; };
+    gtk4.extraConfig = { gtk-application-prefer-dark-theme = 1; };
   };
 
-  home.sessionVariables.GTK_THEME = "Gruvbox-Material-Dark";
+  home.pointerCursor = {
+    gtk.enable = true;
+    x11.enable = true;
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Ice";
+    size = 24;
+  };
 }
