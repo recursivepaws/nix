a1e253dbf79f3ea548a9a085889270d22b5c71ef
Vera Gonzalez <me@vera.lgbt>
2025-10-28 06:08:06 -0400

    basic niri keybinds setup

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -17,6 +17,27 @@
             "Mod+Q".action = spawn "kitty";
             # "Mod+E".action.spawn = "thunar";
             "Mod+X".action = sh "dms ipc call spotlight toggle";
+            "Mod+Shift+X".action = quit;
+            "Mod+F".action = maximize-column;
+            "Mod+Shift+F".action = fullscreen-window;
+            "Mod+C".action = close-window;
+
+            "Mod+H".action = focus-column-left;
+            "Mod+J".action = focus-window-down;
+            "Mod+K".action = focus-window-up;
+            "Mod+L".action = focus-column-right;
+
+            "Mod+Ctrl+H".action = move-column-left;
+            "Mod+Ctrl+J".action = move-window-down;
+            "Mod+Ctrl+K".action = move-window-up;
+            "Mod+Ctrl+L".action = move-column-right;
+
+            # "Mod+Ctrl+S".action = screenshot;
+
+            "Mod+Page_Down".action = focus-workspace-down;
+            "Mod+Page_Up".action = focus-workspace-up;
+            "Mod+Ctrl+Page_Down".action = move-column-to-workspace-down;
+            "Mod+Ctrl+Page_Up".action = move-column-to-workspace-up;
           };
       };
     };
