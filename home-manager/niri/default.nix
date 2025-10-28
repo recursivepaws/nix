b35d531a9862ffe7c348330f960c01c229041336
Vera Gonzalez <me@vera.lgbt>
2025-10-28 05:57:11 -0400

    correct cmd

--- home-manager/niri/default.nix
+++ home-manager/niri/default.nix
@@ -1,4 +1,4 @@
-{ inputs, ... }: {
+{ inputs, config, ... }: {
   imports = [
     inputs.niri.homeModules.niri
     inputs.dankMaterialShell.homeModules.dankMaterialShell.default
@@ -9,7 +9,16 @@
     dankMaterialShell = { enable = true; };
 
     niri = {
-      settings = { spawn-at-startup = [{ command = [ "dms run -d" ]; }]; };
+      settings = {
+        spawn-at-startup = [{ command = [ "dms run -d" ]; }];
+        binds = with config.lib.niri.actions;
+          let sh = spawn "sh" "-c";
+          in {
+            "Mod+Q".action = spawn "kitty";
+            # "Mod+E".action.spawn = "thunar";
+            "Mod+X".action = sh "dms ipc call spotlight toggle";
+          };
+      };
     };
   };
 }
