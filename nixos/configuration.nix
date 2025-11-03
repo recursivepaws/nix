786d1cb79c5159987665d9dfaeae7a43898d9048
Vera Gonzalez <me@vera.lgbt>
2025-11-03 16:01:07 -0500

    add nix-ld for now

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -142,14 +142,10 @@
 
   environment.sessionVariables.NIXOS_OZONE_WL = "1";
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
   programs = {
-    #mtr.enable = true;
-    git = { enable = true; };
+    nix-ld = { enable = true; };
+    git.enable = true;
     lazygit.enable = true;
-    firefox = { enable = true; };
     dankMaterialShell.greeter = {
       enable = true;
       compositor.name = "niri";
