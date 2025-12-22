91fb1da84e4f1747a7f65b10f9b2142cf41f532f
Vera Gonzalez <me@vera.lgbt>
2025-12-22 16:00:46 -0500

    meow

--- home-manager/niri/portal.nix
+++ home-manager/niri/portal.nix
@@ -1,4 +1,20 @@
 { pkgs, ... }: {
-  # Portal configuration moved to system level (nixos/file-manager.nix)
-  # XDG portals must be configured at the system level in NixOS
+  xdg = {
+    portal = {
+      enable = true;
+      config.niri = {
+        default = [ "gnome" "gtk" ];
+        "org.freedesktop.impl.portal.Access" = "gtk";
+        "org.freedesktop.impl.portal.FileChooser" = "gtk";
+        "org.freedesktop.impl.portal.ScreenCast" = "gnome";
+        "org.freedesktop.impl.portal.Secret" = "gnome-keyring";
+      };
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-wlr
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-gnome
+      ];
+    };
+  };
 }
+
--- nixos/file-manager.nix
+++ nixos/file-manager.nix
@@ -7,26 +7,5 @@
     plugins = with pkgs.xfce; [ thunar-volman thunar-archive-plugin ];
   };
 
-  xdg.portal = {
-    enable = true;
-    extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
-      xdg-desktop-portal-gnome
-    ];
-    # config = {
-    #   niri = {
-    #     default = [ "gtk" ];
-    #     "org.freedesktop.impl.portal.FileChooser" = "gtk";
-    #     "org.freedesktop.impl.portal.ScreenCast" = "gnome";
-    #     "org.freedesktop.impl.portal.Secret" = "gnome";
-    #   };
-    #   common = {
-    #     default = "gtk";
-    #     "org.freedesktop.impl.portal.FileChooser" = "gtk";
-    #   };
-    # };
-    wlr.enable = false;
-    xdgOpenUsePortal = true;
-  };
   xdg.mime.defaultApplications = { "inode/directory" = "thunar.desktop"; };
 }
