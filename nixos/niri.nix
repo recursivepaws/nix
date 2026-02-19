3e16a7b2d0356452d2251bffdbbe1604728dc352
Vera Gonzalez <me@vera.lgbt>
2026-02-19 13:10:04 -0500

    replace KDE polkit with GNOME polkit

--- nixos/niri.nix
+++ nixos/niri.nix
@@ -27,6 +27,25 @@
   security.pam.services.gdm.enableGnomeKeyring = true;
   services.gnome.gnome-keyring.enable = true;
 
+  # For some reason the Niri flake uses the KDE polkit agent by default
+  # I prefer the gnome one
+  systemd.user.services.niri-flake-polkit.enable = false;
+  security.polkit.enable = true;
+  systemd.user.services.gnome-polkit-agent = {
+    description = "GNOME PolicyKit Authentication Agent";
+    wantedBy = [ "graphical-session.target" ];
+    wants = [ "graphical-session.target" ];
+    after = [ "graphical-session.target" ];
+    serviceConfig = {
+      Type = "simple";
+      ExecStart =
+        "${pkgs.polkit_gnome}/libexec/polkit-gnome-authentication-agent-1";
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
   xdg.portal = {
     enable = true;
     xdgOpenUsePortal = true;
