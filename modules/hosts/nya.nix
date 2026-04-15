6f53da9181fcd5399f8e4e7622ac46b0eee45e10
Vera Gonzalez <me@vera.lgbt>
2026-04-15 15:56:00 -0400

    rename

--- modules/hosts/nya.nix
+++ modules/hosts/nya.nix
@@ -1,6 +1,5 @@
 { inputs, self, ... }: {
   flake.nixosConfigurations.nya =
     inputs.nixpkgs.lib.nixosSystem { modules = [ self.nixosModules.amd ]; };
-
   flake.nixos.nyaModule = { pkgs, ... }: { boot.loader.grub.enable = true; };
 }
