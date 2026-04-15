38eb94e79763df739b41ab72517bc7f71fceade9
Vera Gonzalez <me@vera.lgbt>
2026-04-15 16:50:24 -0400

    added first real hericium aspect

--- modules/hosts/hericium.nix
+++ modules/hosts/hericium.nix
@@ -1,5 +1,6 @@
-{
+{ den, ... }: {
   den.aspects.hericium = {
+    includes = [ den.aspects.amd ];
     nixos = { pkgs, ... }: { environment.systemPackages = [ pkgs.hello ]; };
     provides.to-users.homeManager = { pkgs, ... }: {
       home.packages = [ pkgs.neovim ];
