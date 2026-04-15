dddcf93cfd8d9a49825b713faa9679278754c342
Vera Gonzalez <me@vera.lgbt>
2026-04-15 15:43:26 -0400

    init

---
+++ modules/amd.nix
@@ -0,0 +1,30 @@
+{ ... }: {
+  flake.nixosModules.amd = { pkgs, ... }: {
+    config = {
+      hardware = {
+        amdgpu = {
+          initrd.enable = true;
+          opencl.enable = true;
+          overdrive = {
+            enable = true;
+            ppfeaturemask = "0xffffffff";
+          };
+        };
+      };
+
+      # Useful userland tools for AMD GPUs
+      environment.systemPackages = with pkgs; [
+        radeontop
+        rocmPackages.rocm-smi
+      ];
+
+      # Fix some ROCm related issues specific to NixOS
+      systemd.tmpfiles.rules = let
+        rocmEnv = pkgs.symlinkJoin {
+          name = "rocm-combined";
+          paths = with pkgs.rocmPackages; [ rocblas hipblas clr ];
+        };
+      in [ "L+    /opt/rocm   -    -    -     -    ${rocmEnv}" ];
+    };
+  };
+}
---
+++ modules/hosts/nya.nix
@@ -0,0 +1,6 @@
+{ inputs, self, ... }: {
+  flake.nixosConfigurations.nya =
+    inputs.nixpkgs.lib.nixosSystem { modules = [ self.nixosModules.amd ]; };
+
+  flake.nixos.nyaModule = { pkgs, ... }: { boot.loader.grub.enable = true; };
+}
