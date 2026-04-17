cbc4512457d5d1c51b40455b51240a7b8bec311a
Vera Gonzalez <me@vera.lgbt>
2026-04-17 17:42:48 -0400

    bitwig theme editor

--- modules/modules/audio/bitwig-patcher.nix
+++
@@ -1,5 +0,0 @@
-{
-  den.aspects.bitwig-patcher = {
-
-  };
-}
--- modules/modules/audio/musical-wine.nix
+++ modules/modules/audio/musical-wine.nix
@@ -1,8 +1,8 @@
-{
+{ den, ... }: {
   den.aspects.musical-wine = {
+    # includes = [ den.aspects.bitwig-theme-editor ];
     homeManager = { pkgs, lib, ... }:
       let
-
         # Wine 9.21 for stability for some plugins
         wine921 = pkgs.wineWow64Packages.yabridge;
 
@@ -126,12 +126,66 @@
         #
         # };
 
+        bitwig-theme-editor = let
+          description = "Bitwig Theme Editor";
+          pname = "bitwig-theme-editor";
+          version = "2.3.1";
+        in pkgs.stdenv.mkDerivation rec {
+          inherit pname version;
+          src = let filename = "${pname}-${version}-hotfix.jar";
+          in pkgs.fetchurl {
+            name = filename;
+            url =
+              "https://github.com/Berikai/${pname}/releases/download/${version}/${filename}";
+            hash = "sha256-Fkz1s5DY8Ey3Il7CBHNmV87JyJ8clSEF1CltVwlq3/w=";
+          };
+
+          nativeBuildInputs = with pkgs; [ jre makeWrapper copyDesktopItems ];
+
+          # Skip the normal build phases — there's nothing to compile
+          dontUnpack = true;
+          dontBuild = true;
+
+          desktopItems = [
+            (pkgs.makeDesktopItem {
+              name = pname;
+              exec = pname;
+              icon = pname;
+              desktopName = description;
+              comment = description;
+              categories = [ "Utility" ];
+            })
+          ];
+
+          installPhase = ''
+            # Install the JAR
+            mkdir -p $out/share/java
+            cp $src $out/share/java/${pname}.jar
+
+            # Create a wrapper script in bin/
+            mkdir -p $out/bin
+            makeWrapper ${pkgs.jre}/bin/java $out/bin/${pname} \
+              --add-flags "-jar $out/share/java/${pname}.jar"
+          '';
+
+          meta = {
+            description = description;
+            mainProgram = pname;
+          };
+        };
+
         bitwig6-local = pkgs.stdenv.mkDerivation {
           pname = "bitwig-studio-local";
           version = "6.0";
           src = bitwig-studio-deb;
 
-          nativeBuildInputs = [ pkgs.dpkg pkgs.makeWrapper ];
+          nativeBuildInputs = with pkgs; [
+            dpkg
+            makeWrapper
+            jdk
+            bitwig-theme-editor
+          ];
+
           unpackPhase = "dpkg-deb -x $src .";
 
           installPhase = ''
@@ -143,17 +197,15 @@
             # COPY THE ICONS AND DESKTOP FILES (This was missing!)
             cp -r usr/share/* $out/share/
 
-            # TODO: patching
+            # Overwrite original JAR with your patched one
+            # rm $out/libexec/bin/bitwig.jar
+            # Patch the bitwig jar to use the theme editor
+            java -jar ${bitwig-theme-editor}/share/java/bitwig-theme-editor.jar $out/libexec/bin/bitwig.jar
 
             # Link the binary
             ln -s $out/libexec/bitwig-studio $out/bin/bitwig-studio
           '';
         };
-
-        # # Overwrite original JAR with your patched one
-        # rm $out/libexec/bin/bitwig.jar
-        # # Patch the bitwig jar to use the theme editor
-        # java -jar ${bitwig-theme-editor-jar} $out/libexec/bin/bitwig.jar
 
         # Bitwig FHS
         bitwig-fhs = pkgs.buildFHSEnv {
@@ -275,6 +327,7 @@
         '';
       in {
         home.packages = [
+          bitwig-theme-editor
           bitwig-fhs
           wine-router
           wineserver-router
