5f56ae122a08d438a799f6bef3e26040fdde3b2e
Vera Gonzalez <me@vera.lgbt>
2026-05-04 17:51:24 -0400

    added yubikey & removed old fungi

--- modules/default/ragenix.nix
+++ modules/default/ragenix.nix
@@ -5,15 +5,36 @@
     inputs.nixpkgs.follows = "nixpkgs";
   };
 
+  # den.default.homeManager =
+  #   { pkgs, ... }:
+  #   {
+  #     home.sessionPath = with pkgs; [ "${age-plugin-yubikey}/bin" ];
+  #   };
   den.default.nixos =
     { pkgs, ... }:
+    let
+      ageBin = pkgs.writeShellScript "age-with-plugins" ''
+        export PATH="${pkgs.age-plugin-yubikey}/bin:$PATH"
+        exec ${pkgs.rage}/bin/rage "$@"
+      '';
+    in
     {
       imports = [ inputs.agenix.nixosModules.default ];
-      environment.systemPackages = with pkgs; [ ragenix ];
-      age.identityPaths = [
-        "/etc/ssh/ssh_host_ed25519_key"
-        # "/home/vera/.ssh/id_ed25519"
+
+      environment.systemPackages = with pkgs; [
+        rage
+        ragenix
+        age-plugin-yubikey
       ];
+
+      services.pcscd.enable = true;
+
+      age.ageBin = "${ageBin}";
+      age.identityPaths = [
+        ../../assets/age-yubikey-identity-8f6fdf41.txt
+        "/etc/ssh/ssh_host_ed25519_key"
+      ];
+
       age.secrets = {
         startup = {
           file = ../../secrets/startup.age;
--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -1,17 +1,10 @@
 let
-  network = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHBh6YdnTvVsovh6jX65ub96Qls5Y4Dl/pGBOFoi9TWh";
-  signing = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIINY5xZYRlbxjdw4N47VADFRSU3EeSI3Yze97F8cWGLS";
-  vera = [
-    network
-    signing
-  ];
-
-  hericium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHc1lBH+WMR9uqtNaCBxheZBtQv7py+Cn9+vNaU3hmfa";
-  computers = [
-    hericium
-  ];
+  yubikey-totoro = "age1yubikey1qwtem02lwn7jpfwl5mu4jsrvty8xhv7s5vn005530apnhjpmx8r8sup2ve0";
+  host-hericium = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHc1lBH+WMR9uqtNaCBxheZBtQv7py+Cn9+vNaU3hmfa";
 in
 {
-  "user.age".publicKeys = vera;
-  "startup.age".publicKeys = computers;
+  "startup.age".publicKeys = [
+    yubikey-totoro
+    host-hericium
+  ];
 }
