dfb68b321cc432b8ca1daf74f14d53ed05bc0cff
Vera Gonzalez <me@vera.lgbt>
2026-05-05 10:25:28 -0400

    remove old config

--- _nixos/stylix.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, pkgs, ... }: {
-  imports = with inputs; [ stylix.nixosModules.stylix ];
-  stylix = {
-    polarity = "dark";
-    targets.gtk.enable = false;
-    autoEnable = true;
-    base16Scheme = "${pkgs.nightfox-nvim}/extra/carbonfox/base16.yaml";
-  };
-}
--- _overlays/default.nix
+++
@@ -1,5 +0,0 @@
-# This file defines overlays
-{ inputs, ... }: {
-  nixpkgs.overlays =
-    [ inputs.niri-flake.overlays.niri inputs.claude-code.overlays.default ];
-}
