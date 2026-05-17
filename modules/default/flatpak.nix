f6e511b15cf5269efd4dd4e6448e0573bd5605ce
Vera Gonzalez <me@vera.lgbt>
2026-05-16 20:18:23 -0400

    add the fastmail flatpak

--- modules/default/flatpak.nix
+++ modules/default/flatpak.nix
@@ -3,6 +3,11 @@
   flake-file.inputs.nix-flatpak.url = "github:gmodena/nix-flatpak?ref=latest";
   den.default.nixos = {
     imports = [ inputs.nix-flatpak.nixosModules.nix-flatpak ];
-    services.flatpak.enable = true;
+    services.flatpak = {
+      enable = true;
+      packages = [
+        "com.fastmail.Fastmail"
+      ];
+    };
   };
 }
