7c0c6a8393737b696d548047cd383b149e96bb15
Vera Gonzalez <me@vera.lgbt>
2026-07-17 16:49:37 -0400

    add vulkan to chrome so wgpu works

--- modules/modules/browser.nix
+++ modules/modules/browser.nix
@@ -14,6 +14,7 @@
         programs.google-chrome = {
           enable = true;
           # commandLineArgs = [ "--load-extension=${bilibili-to-english}" ];
+          commandLineArgs = [ "--enable-features=Vulkan" ];
         };
       };
   };
