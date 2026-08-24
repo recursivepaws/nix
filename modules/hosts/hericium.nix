fe7a98f6b787097a53525fa0c5dd7180048f2682
Vera Gonzalez <me@vera.lgbt>
2026-08-24 14:18:34 -0400

    hacky bluetooth fix for hericium

--- modules/hosts/hericium.nix
+++ modules/hosts/hericium.nix
@@ -24,6 +24,28 @@
           enable = true;
           enableVirtualCamera = true;
         };
+
+        services = {
+          # BT headphones: A2DP output only, no HFP/HSP, no profile autoswitch.
+          # Stops playback falling back to mono HFP "mic mode".
+          # Headset mic unusable as a result; StreamCam is the input anyway.
+          pipewire.wireplumber.extraConfig."51-bluez-headphones" = {
+            "wireplumber.settings" = {
+              "bluetooth.autoswitch-to-headset-profile" = false;
+            };
+            "monitor.bluez.properties" = {
+              "bluez5.roles" = [ "a2dp_source" ];
+              "bluez5.enable-sbc-xq" = true;
+            };
+          };
+
+          # Deauthorize flaky Intel radio (see modules/default/bluetooth.nix).
+          # BlueZ then only sees the external Realtek dongle.
+          # hericium-only: amanita needs the Intel radio. No dongle = no BT.
+          udev.extraRules = ''
+            ACTION=="add", SUBSYSTEM=="usb", ATTR{idVendor}=="8087", ATTR{idProduct}=="0aa7", ATTR{authorized}="0"
+          '';
+        };
       };
 
     provides.to-users.includes = with den.aspects; [
