ae45637229a1149ab793f6c216a5a9a2dc4b456d
Vera Gonzalez <me@vera.lgbt>
2026-08-25 09:50:11 -0400

    update obsidian

--- modules/aspects/obsidian.nix
+++ modules/aspects/obsidian.nix
@@ -1,4 +1,6 @@
 { inputs, ... }: {
+  # NOTE: nice reference https://github.com/nyxar77/homeconfig/blob/15ce1d4d2ee505b0701c34c0b59972146561c9fc/home/modules/apps/obsidian.nix
+
   flake-file.inputs.obsidian-extensions = {
     url = "github:karaolidis/nix-obsidian-extensions";
     inputs.nixpkgs.follows = "nixpkgs";
@@ -11,15 +13,30 @@
       ];
       programs.obsidian = {
         enable = true;
+        vaults = {
+          # notes = {
+          #   target = "Documents/notes";
+          # };
+          tickets = {
+            target = "Software/tickets";
+          };
+        };
         defaultSettings = {
+          app = {
+            vimMode = true;
+            trashOption = "system";
+            defaultViewMode = "preview";
+          };
           communityPlugins = with pkgs.obsidianPlugins; [
             obsidian-git
+            obsidian-tasks-plugin
             dataview
             table-editor-obsidian
             sansconverter
             omnisearch
             templater-obsidian
             obsidian-linter
+            heatmap-tracker
           ];
           themes = with pkgs.obsidianThemes; [
             carbon
