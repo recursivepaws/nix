12dbb700c3671df0ad613c9e26236dbb69a54526
Vera Gonzalez <me@vera.lgbt>
2026-09-14 23:02:44 -0400

    REMOVE NH AND RESTREAM

--- modules/nh.nix
+++ modules/nh.nix
@@ -6,19 +6,4 @@
     {
       packages = den.lib.nh.denPackages { fromFlake = true; } pkgs;
     };
-
-  den.default.nixos =
-    { ... }:
-    {
-      # TODO: unfuck this
-      programs.nh = {
-        enable = false;
-        clean = {
-          enable = false;
-          # dates = "daily";
-          # # TODO: (https://github.com/nix-community/nh/issues/722): on nh 4.4.0 the clean
-          # extraArgs = "--keep 10 --keep-since 5d --no-gcroots";
-        };
-      };
-    };
 }
