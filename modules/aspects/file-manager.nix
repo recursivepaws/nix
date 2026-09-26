c1d1ae92a5a52e2b77add8c19aef775890be108d
Vera Gonzalez <me@vera.lgbt>
2026-09-26 07:14:23 -0400

    add default mimes

--- modules/aspects/file-manager.nix
+++ modules/aspects/file-manager.nix
@@ -1,16 +1,64 @@
 {
   den.aspects.file-manager = {
     nixos =
-      { pkgs, ... }:
+      { pkgs, lib, ... }:
+      let
+        # "image" "eog.desktop" [ "png" "jpeg" ] -> { "image/png" = "eog.desktop"; ... }
+        assoc =
+          prefix: app: subtypes:
+          lib.genAttrs (map (t: "${prefix}/${t}") subtypes) (_: app);
+      in
       {
         services.gvfs.enable = true;
         environment.systemPackages = with pkgs; [
           nautilus
           file-roller
         ];
-        xdg.mime.defaultApplications = {
-          "inode/directory" = "org.gnome.Nautilus.desktop";
-        };
+        xdg.mime.defaultApplications = lib.mergeAttrsList [
+          {
+            "inode/directory" = "org.gnome.Nautilus.desktop";
+            "application/pdf" = "google-chrome.desktop";
+          }
+          (assoc "image" "org.gnome.eog.desktop" [
+            "png"
+            "jpeg"
+            "gif"
+            "webp"
+            "tiff"
+            "bmp"
+            "svg+xml"
+            "heif"
+            "avif"
+          ])
+          (assoc "video" "vlc.desktop" [
+            "mp4"
+            "mpeg"
+            "webm"
+            "quicktime"
+            "x-matroska"
+            "x-msvideo"
+            "x-flv"
+          ])
+          (assoc "audio" "vlc.desktop" [
+            "mpeg"
+            "flac"
+            "ogg"
+            "x-wav"
+            "x-m4a"
+            "aac"
+            "opus"
+          ])
+          (assoc "application" "org.gnome.FileRoller.desktop" [
+            "zip"
+            "x-tar"
+            "gzip"
+            "x-bzip2"
+            "x-xz"
+            "zstd"
+            "x-7z-compressed"
+            "vnd.rar"
+          ])
+        ];
       };
   };
 }
